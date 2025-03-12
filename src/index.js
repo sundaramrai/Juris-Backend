77c6a2b6f4103d3b3e56f8c1abbc626217cac4bb
Sundaram Rai <sundaramrai02@gmail.com>
2025-03-12 09:51:57 +0530

    update origin

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-  origin: "*",
+  origin: ["http://localhost:4200", "https://juris-legal-ai.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
