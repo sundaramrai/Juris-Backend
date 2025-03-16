727be2a2a84c41ecfb053afd46fb79859a00e86e
Sundaram Rai <sundaramrai02@gmail.com>
2025-03-16 16:40:44 +0530

    remove deprecated mongoose connection options

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,6 @@ const MONGO_URI = process.env.MONGO_URI;
 const connectDatabase = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
     });
     console.log("✅ MongoDB Connected Successfully");
