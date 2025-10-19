bf130af5afcd7e765e75b1dc5845c5ec3cbd0979
Sundaram Rai <sundaramrai02@gmail.com>
2025-10-19 19:36:46 +0530

    remove: delete OTP request and verification routes from authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,7 +6,5 @@ const { authenticateToken } = require("../middleware/auth");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/user", authenticateToken, authController.getUser);
-router.post("/register/request-otp", authController.requestOTP);
-router.post("/register/verify-otp", authController.verifyOTP);
 
 module.exports = router;
