49d26e52965a182e5382085f7ec147182c7a577f
Sundaram Rai <sundaramrai02@gmail.com>
2025-11-09 17:57:27 +0530

    migrate validation utility to ES6 module syntax

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,13 +6,13 @@ function validateEnvVars() {
     "ENCRYPTION_KEY",
     "USERNAME_HASH_SALT",
     "GOOGLE_SEARCH_ENGINE_ID",
-    "GOOGLE_SEARCH_API_KEY"
+    "GOOGLE_SEARCH_API_KEY",
   ];
-  const missingVars = requiredEnvVars.filter(key => !process.env[key]);
+  const missingVars = requiredEnvVars.filter((key) => !process.env[key]);
   if (missingVars.length > 0) {
-    console.error("Missing environment variables:", missingVars.join(', '));
+    console.error("Missing environment variables:", missingVars.join(", "));
     process.exit(1);
   }
 }
 
-module.exports = { validateEnvVars };
+export { validateEnvVars };
