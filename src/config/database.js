e0a628dc8b6297ad8623b2943547f2dd15532737
Sundaram Rai <sundaramrai02@gmail.com>
2025-11-09 17:58:48 +0530

    migrate database.js to ES6 module syntax

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,19 @@
-// src/config/database.js
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
 const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       maxPoolSize: 10,
       socketTimeoutMS: 45000,
       connectTimeoutMS: 30000,
-      serverSelectionTimeoutMS: 30000
+      serverSelectionTimeoutMS: 30000,
     });
-    console.log('MongoDB connected successfully');
+    console.log("MongoDB connected successfully");
     return mongoose.connection;
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     throw error;
   }
 };
 
-module.exports = { connectDatabase };
+export { connectDatabase };
