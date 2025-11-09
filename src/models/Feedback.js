b731523af37e531ed35b663fdb23528573335bab
Sundaram Rai <sundaramrai02@gmail.com>
2025-11-09 17:56:02 +0530

    migrate Feedback model to ES6 module syntax

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const FeedbackSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  feedback: [{
-    satisfaction: { type: Number, required: true, min: 1, max: 5 },
-    issues: { type: String, required: true },
-    improvements: { type: String, required: true },
-  }]
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  feedback: [
+    {
+      satisfaction: { type: Number, required: true, min: 1, max: 5 },
+      issues: { type: String, required: true },
+      improvements: { type: String, required: true },
+    },
+  ],
 });
 
-module.exports = mongoose.model('Feedback', FeedbackSchema);
+export default mongoose.model("Feedback", FeedbackSchema);
