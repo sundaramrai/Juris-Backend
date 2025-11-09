9bbbcb61f8e528d1c8213f88fd649521fa5d24fa
Sundaram Rai <sundaramrai02@gmail.com>
2025-11-09 17:56:30 +0530

    migrate chatRoutes to ES6 module syntax

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,13 +1,26 @@
-const express = require("express");
+import express from "express";
+import * as chatController from "../controllers/chatController.js";
+import { authenticateToken } from "../middleware/auth.js";
+
 const router = express.Router();
-const chatController = require("../controllers/chatController");
-const { authenticateToken } = require("../middleware/auth");
 
 router.post("/", authenticateToken, chatController.processChat);
 router.get("/all", authenticateToken, chatController.getAllChats);
-router.get("/history/:chatId", authenticateToken, chatController.getChatHistory);
-router.delete("/history/:chatId", authenticateToken, chatController.clearChatHistory);
+router.get(
+    "/history/:chatId",
+    authenticateToken,
+    chatController.getChatHistory
+);
+router.delete(
+    "/history/:chatId",
+    authenticateToken,
+    chatController.clearChatHistory
+);
 router.post("/new", authenticateToken, chatController.createNewChat);
-router.patch("/title/:chatId", authenticateToken, chatController.updateChatTitle);
+router.patch(
+    "/title/:chatId",
+    authenticateToken,
+    chatController.updateChatTitle
+);
 
-module.exports = router;
+export default router;
