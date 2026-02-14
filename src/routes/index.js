449ccb00a830e94e3cc9376a6b54fb8bac7c6daf
Sundaram Rai <sundaramrai02@gmail.com>
2026-02-14 23:07:48 +0530

    remove feedback route from main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import authRoutes from "./authRoutes.js";
 import chatRoutes from "./chatRoutes.js";
-import feedbackRoutes from "./feedbackRoutes.js";
 import healthCheckRoutes from "./healthCheck.js";
 
 const router = express.Router();
 
 router.use("/", authRoutes);
 router.use("/chat", chatRoutes);
-router.use("/feedback", feedbackRoutes);
 router.use("/health", healthCheckRoutes);
 
 export default router;
